dda8b5d730c60c2e28e3deeb2eb156a367e28367
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-03-30 17:27:20 -0400

    Update playwright.config.ts
    Commented out mobile device types for running playwright tests on

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
 
-    /* Test against mobile viewports. */
+    /* Test against mobile viewports.
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 7'] },
