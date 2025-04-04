e54f813eefc77de8e158cead81e7e223a6e61640
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-04-04 17:36:54 -0400

    Update playwright.config.ts
    Updated file to specify base_url to run tests using as localhost:8080

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  
   // Location for playwright test files to run
   testDir: './__tests__/lib',
   /* Run tests in files in parallel */
