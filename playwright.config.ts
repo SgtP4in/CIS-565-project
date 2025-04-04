567c6696a61d45df1c4569aa6cb6083eae7bd192
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-04-04 17:36:06 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // Location for playwright test files to run
   testDir: './__tests__/lib',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -28,8 +29,9 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    /* Base URL to use in actions like `await page.goto('/')`. 
+       Port:8080 as defined in Github actions yaml file */
+    baseURL: 'http://127.0.0.1:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
